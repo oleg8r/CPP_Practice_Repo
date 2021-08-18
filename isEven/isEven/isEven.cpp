a82a709f254da488d3f76084bb66ecdd318d5240
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2021-08-18 09:18:54 -0400

    Update isEven.cpp

--- isEven/isEven/isEven.cpp
+++ isEven/isEven/isEven.cpp
@@ -24,6 +24,6 @@ int main()
 //   2. Use the Team Explorer window to connect xxx
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the xxx
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
