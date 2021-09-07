c06dee8075e17fb9a6ea4defbdda74f28f776212
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2021-09-07 12:29:18 -0400

    Update isEven.cpp

--- isEven/isEven/isEven.cpp
+++ isEven/isEven/isEven.cpp
@@ -21,7 +21,7 @@ int main()
 
 
 
-//   2. Use the Team Explorer window to xxx xxx
+//   2. Use the Team Explorer window xxx xxx xxx
 //   3. Use the Output window to see build output and xxx xxx
 //   4. Use the Error List window to view xxx
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to xxx xxx
