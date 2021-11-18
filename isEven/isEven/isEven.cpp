9e392ba06912ce658392c8f967f3e2437f8d553f
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2021-11-17 20:41:10 -0500

    Update isEven.cpp

--- isEven/isEven/isEven.cpp
+++ isEven/isEven/isEven.cpp
@@ -25,5 +25,5 @@ int main()
 //   3. Use the Output window to see xxx xxx xxx xxx xxx
 //   4. Use the Error List window to view xxx
 //   5. Go to Project > Add New Item to create new xxx xxx, or Project > Add Existing Item to add existing code xxx xxx xxx xxx
-//   6. In the future, to xxx xxx xxx xxx, xxx xxx xxx > xxx > xxx xxx xxx xxx .xxx xxx
+//   6. In the xxx, to xxx xxx xxx xxx, xxx xxx xxx > xxx > xxx xxx xxx xxx .xxx xxx
 
