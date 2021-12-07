832dab2916eabf40a842c1d14b3c3127cc9b7ae9
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2021-12-06 19:00:56 -0500

    Update isEven.cpp

--- isEven/isEven/isEven.cpp
+++ isEven/isEven/isEven.cpp
@@ -22,7 +22,7 @@ int main()
 
 
 //   2. First step: Apply the Team's window and open the menu
-//   3. Use the first window to see a various options for adjusting the interface xxx xxx xxx xxx
+//   3. Use the first window to see a various options for adjusting the interface xxx xxx xxx
 //   4. Use the Error List window to view xxx
 //   5. Go to Project > Add New Item to create new xxx xxx, or Project > Add Existing Item to add existing code xxx xxx xxx xxx
 //   6. Lat step xxx xxx, to xxx xxx xxx xxx, xxx xxx xxx > xxx > xxx xxx xxx xxx .xxx xxx
