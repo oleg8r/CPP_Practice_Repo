8c2513358dd5af3c599f1afd4193c38cce1d62d9
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2022-02-22 23:40:42 -0500

    Update isEven.cpp

--- isEven/isEven/isEven.cpp
+++ isEven/isEven/isEven.cpp
@@ -1,5 +1,5 @@
 // isEven.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// Checks if the value is even
+// Checks if the value is even or odd
 
 #include <iostream>
 
