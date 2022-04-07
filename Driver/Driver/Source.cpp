1ece91b68eb1b3c436707e9481d6e9fc5e814f70
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2022-04-07 17:35:22 -0400

    Update Source.cpp

--- Driver/Driver/Source.cpp
+++ Driver/Driver/Source.cpp
@@ -9,7 +9,7 @@ struct Driver
 	string lastName;		// driver's last name field
 	char carCategory = 0;		// car category field
 	//char carCategory{ 0 };	// car category field
-	int drivingXP = 0;		// driver's experience field
+	int drivingXP = 0;		// Driver's experience field
 	//char drivingXP{ 0 };		// Car category field
 };
 
