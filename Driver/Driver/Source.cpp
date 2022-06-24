3cf9545c5fa8ed11df5d00fa7f2445d7e9426dc3
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2022-06-23 20:45:13 -0400

    Update Source.cpp

--- Driver/Driver/Source.cpp
+++ Driver/Driver/Source.cpp
@@ -22,7 +22,7 @@ int main()
 	cout << "Enter ## of drivers" << endl;
 	cin >> n;
 
-	Driver* list = new Driver[n]; //creates a dynamic array for "the list of drivers" for n cars
+	Driver* list= new Driver[n]; //creates a dynamic array for "the list of drivers" for n cars
 	// Fills out the array of structures of "Driver" type
 	for (int i = 0; i < n; i++)
 	{
