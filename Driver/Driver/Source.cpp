8320fbabd5d4e43600adc75dbc954dae942503b6
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2022-07-25 11:50:04 -0400

    Update Source.cpp

--- Driver/Driver/Source.cpp
+++ Driver/Driver/Source.cpp
@@ -1,4 +1,4 @@
-// This program is designed to work with information about drivers
+// This program is developed to work with the information about drivers
 
 #include <iostream>
 using namespace std;
